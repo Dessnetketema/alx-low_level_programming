0158eb07e0f9cdd30ebf44455cedca851983ffe1
Dessnetketema <dessnet79@gmail.com>
2022-09-01 08:17:47 -0700

    Print lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,19 +1,21 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
- * @h: the head of a double linked list.
- * Return: the number of nodes.
+ * print_dlistint - Prints all elements of ll
+ *
+ * @h: Head dlistint_t
+ *
+ * Return: size_t
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	unsigned int count = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nodes++;
+		count++;
 	}
-	return (nodes);
+	return (count);
 }
